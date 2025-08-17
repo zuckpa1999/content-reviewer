0e459980e9c9d4ed4c56468b2440fd958d12c975
zuck_pa <zuckpa1999@gmail.com>
2025-08-17 19:59:14 +0700

    fix: call handleAddReview when click + button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
         <button className=" inline-flex h-10 w-10 items-center 
         justify-center rounded-full bg-gray-50 text-black drop-shadow-sm transition-colors duration-150 hover:bg-gray-200"
-          onClick={() => setShowModal(true)}>
+          onClick={() => handleAddReview()}>
           <svg className="w-6 h-6 text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
           </svg>
